db7d028ce0e17ebfa14c6ae8cac8fe798ea0a776
Jorge Gil <gil.jorge@gmail.com>
2017-01-16 14:50:33 +0100

    first commit

---
+++ KPOpilot/kpo_dataexplorer.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ KPOpilotDockWidget
+                                 A QGIS plugin
+ Knooppunten Datasysteem
+                             -------------------
+        begin                : 2016-12-19
+        git sha              : $Format:%H$
+        copyright            : (C) 2016 by Jorge Gil
+        email                : gil.jorge@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
+import os
+
+from PyQt4 import QtCore, QtGui, uic
+
+from qgis.core import *
+from qgis.gui import *
+
+
+
+class KPOExplorer(QtCore.QObject):
+
+    def __init__(self, iface, dockwidget):
+
+        self.iface = iface
+        self.dlg = dockwidget
+
+        print 'Hello world!'
---
+++ KPOpilot/kpo_datasysteem_pilot_dockwidget.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ KPOpilotDockWidget
+                                 A QGIS plugin
+ Knooppunten Datasysteem
+                             -------------------
+        begin                : 2016-12-19
+        git sha              : $Format:%H$
+        copyright            : (C) 2016 by Jorge Gil
+        email                : gil.jorge@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
+import os
+
+from PyQt4 import QtGui, uic
+from PyQt4.QtCore import pyqtSignal
+
+
+FORM_CLASS, _ = uic.loadUiType(os.path.join(
+    os.path.dirname(__file__), 'kpo_datasysteem_pilot_dockwidget_base.ui'))
+
+
+class KPOpilotDockWidget(QtGui.QDockWidget, FORM_CLASS):
+
+    closingPlugin = pyqtSignal()
+
+    def __init__(self, parent=None):
+        """Constructor."""
+        super(KPOpilotDockWidget, self).__init__(parent)
+        # Set up the user interface from Designer.
+        # After setupUI you can access any designer object by doing
+        # self.<objectname>, and you can use autoconnect slots - see
+        # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
+        # #widgets-and-dialogs-with-auto-connect
+        self.setupUi(self)
+
+        self.plugin_dir = os.path.dirname(__file__)
+
+
+        self.logosLabel.setPixmap(QtGui.QPixmap(self.plugin_dir + '/images/partner_logos.png'))
+
+
+
+    def closeEvent(self, event):
+        self.closingPlugin.emit()
+        event.accept()
+
