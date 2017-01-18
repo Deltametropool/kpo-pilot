32fb490d86948f7a6f3d8bb553812d9f34f15f25
Laurens Versluis <laurens_versluis@hotmail.com>
2017-01-18 22:14:13 +0100

    Day 1

--- KPOpilot/kpo_dataexplorer.py
+++ KPOpilot/kpo_dataexplorer.py
@@ -39,6 +39,8 @@ class KPOExplorer(QtCore.QObject):
         self.iface = iface
         self.dlg = dockwidget
         self.plugin_dir = plugin_dir
+        self.canvas = self.iface.mapCanvas()
+
 
         # self.dlg.button.clicked.connect(self.updateLayers)
 
@@ -49,11 +51,46 @@ class KPOExplorer(QtCore.QObject):
         pass
 
     def readDataModel(self):
-
         self.iface.project.read(QFileInfo(self.plugin_dir + 'project'))
+        return self.iface.project.read() # True if successful
+
+    def createMapTip(self, layer, fields, mouse_location):
+        self.tip = QgsMapTip()
+
+
+    def changeMapTip(self):
         pass
 
+    def showMapTip(self):
+        if self.canvas.underMouse():
 
+            pointQgs = self.lastMapPosition
+            pointQt = self.canvas.mouseLastXY()
+            self.canvas.showMapTip(self.layer, pointQgs, pointQt,
+                                   self.canvas)
+
+    def setFeatureSelection(self, features, layer):
+        if features:
+            if layer.isValid():
+                layer.setSelectedFeatures(features)
+
+    def setExtentToLayer(self,layer):
+        if layer.isValid():
+            self.canvas.setExtent(layer.extent())
+            self.canvas.refresh()
+
+    def setExtentToSelection(self,layer):
+        if layer.isValid():
+            if layer.selectedFeatures():
+                self.canvast.setExtent(layer.boundingBoxOfSelected())
+                self.canvas.refresh()
+
+    def addLayersToCanvas(self, layers):
+        # add layer to the registry
+        self.QgsMapLayerRegistry.instance().addMapLayer(layer)
+        # set the map canvas layer set
+        self.canvas.setLayerSet([QgsMapCanvasLayer(layer)])
+        pass
 
 
 
