ede91eb63f1817bbce247792ccbc0c4da947ec22
Jorge Gil <gil.jorge@gmail.com>
2017-02-13 17:49:16 +0100

    translated data model and UI

--- scripts/kpo_datamodel.sql
+++ scripts/kpo_datamodel.sql
@@ -7,141 +7,152 @@
 CREATE SCHEMA datasysteem;
 ALTER SCHEMA datasysteem OWNER TO postgres;
 
--- DROP TABLE IF EXISTS datasysteem.housing_scenarios CASCADE;
-CREATE TABLE datasysteem.housing_scenarios(
+-- DROP TABLE IF EXISTS datasysteem.woonscenarios CASCADE;
+CREATE TABLE datasysteem.woonscenarios(
 	sid serial NOT NULL,
 	geom geometry(MultiPolygon,28992),
 	postcode varchar,
-	place_name varchar,
-	scenario_name varchar,
-	within_walking_dist boolean,
-	within_cycling_dist boolean,
-	nearest_station varchar,
-	households integer,
+	plaatsnaam varchar,
+	scenario_naam varchar,
+	op_loopafstand boolean,
+	op_fietsafstand boolean,
+	dichtstbijzijnde_station varchar,
+	huishoudens integer,
 	area double precision,
-	density double precision,
-	new_households integer,
-	percent_change double precision,
-	CONSTRAINT housing_scenarios_pk PRIMARY KEY (sid)
+	dichtheid double precision,
+	nieuwe_huishoudens integer,
+	procentuele_verandering double precision,
+	CONSTRAINT woonscenarios_pk PRIMARY KEY (sid)
 );
 
--- DROP TABLE IF EXISTS datasysteem.housing_summary CASCADE;
-CREATE TABLE datasysteem.housing_summary(
+-- DROP TABLE IF EXISTS datasysteem.overzicht_woonscenarios CASCADE;
+CREATE TABLE datasysteem.overzicht_woonscenarios(
 	sid serial NOT NULL,
-	scenario_name varchar,
-	policy_level smallint,
-	new_households integer,
-	within_walking_dist integer,
-	within_cycling_dist integer,
-	outside_influence integer,
-	CONSTRAINT housing_summary_pkey PRIMARY KEY (sid)
+	scenario_naam varchar,
+	tod_beleidsniveau smallint,
+	verwachte_huishoudens integer,
+	op_loopafstand integer,
+	op_fietsafstand integer,
+	buiten_invloedsgebied integer,
+	CONSTRAINT overzicht_woonscenarios_pkey PRIMARY KEY (sid)
 );
 
--- DROP TABLE IF EXISTS datasysteem.transit_nodes CASCADE;
-CREATE TABLE datasysteem.transit_nodes(
+-- DROP TABLE IF EXISTS datasysteem.knooppunten CASCADE;
+CREATE TABLE datasysteem.knooppunten(
 	sid serial NOT NULL,
 	geom geometry(MultiPoint,28992),
-	station_name varchar,
-	scenario_name varchar,
-	policy_level smallint,
-	users integer,
-	users_diff integer,
-	users_pc_change double precision,
-	bicycle_parking integer,
-	bicycle_occupation integer,
-	bicycle_occupation_diff double precision,
-	bicycle_pc_change double precision,
-	platform integer,
-	platform_diff double precision,
-	platform_pc_change double precision,
-	stairs integer,
-	stairs_diff double precision,
-	stairs_pc_change double precision,
-	user_flows integer,
-	user_flows_diff double precision,
-	user_flows_pc_change double precision,
-	cycle_routes varchar,
-	public_transport_routes varchar,
-	CONSTRAINT transit_nodes_pkey PRIMARY KEY (sid)
+	station_naam varchar,
+	scenario_naam varchar,
+	tod_beleidsniveau smallint,
+	huishoudens integer,
+	in_uitstappers integer,
+	in_uitstappers_verschil integer,
+	in_uitstappers_pc_verandering double precision,
+	fietsparkeerplaatsen integer,
+	fietsenstalling_capaciteit integer,
+	fietsenstalling_verschil double precision,
+	fietsenstalling_pc_verandering double precision,
+	perron_capaciteit integer,
+	perron_verschil double precision,
+	perron_pc_verandering double precision,
+	stijgpunten_capaciteit integer,
+	stijgpunten_verschil double precision,
+	stijgpunten_pc_verandering double precision,
+	loopstromen_capaciteit integer,
+	loopstromen_verschil double precision,
+	loopstromen_pc_verandering double precision,
+	fietsroutes varchar,
+	ov_routes varchar,
+	CONSTRAINT knooppunten_pkey PRIMARY KEY (sid)
 );
 
--- DROP TABLE IF EXISTS datasysteem.spatial_characteristics CASCADE;
-CREATE TABLE datasysteem.spatial_characteristics(
+-- DROP TABLE IF EXISTS datasysteem.ruimtelijke_kenmerken CASCADE;
+CREATE TABLE datasysteem.ruimtelijke_kenmerken(
 	sid serial NOT NULL,
 	geom geometry(MultiPolygon,28992),
 	cell_id varchar,
-	households integer,
-	residents integer,
-	intensity integer,
-	built_density double precision,
-	property_value double precision,
-	pta_level varchar,
-	pta_index double precision,
-	CONSTRAINT spatial_characteristics_pkey PRIMARY KEY (sid)
+	huishoudens integer,
+	inwoners integer,
+	intensiteit integer,
+	fysieke_dichtheid double precision,
+	woz_waarde double precision,
+	ov_bereikbaarheidsniveau varchar,
+	ov_bereikbaarheidsindex double precision,
+	CONSTRAINT ruimtelijke_kenmerken_pkey PRIMARY KEY (sid)
 );
 
--- DROP TABLE IF EXISTS datasysteem.development_plans CASCADE;
-CREATE TABLE datasysteem.development_plans(
+-- DROP TABLE IF EXISTS datasysteem.ontwikkellocaties CASCADE;
+CREATE TABLE datasysteem.ontwikkellocaties(
 	sid serial NOT NULL,
 	geom geometry(MultiPolygon,28992),
-	plan_name varchar,
-	site_id varchar,
-	municipality varchar,
-	site_name varchar,
-	address varchar,
-	built_dwellings integer,
-	planned_dwellings integer,
-	net_dwellings integer,
-	area double precision,
-	density double precision,
-	mean_ptal double precision,
-	max_ptal double precision,
-	CONSTRAINT development_locations_pkey PRIMARY KEY (sid)
+	plan_naam varchar,
+	plan_id varchar,
+	gemeente varchar,
+	plaatsnaam varchar,
+	adres varchar,
+	bestaande_woningen integer,
+	geplande_woningen integer,
+	net_nieuwe_woningen integer,
+	vlakte double precision,
+	dichtheid double precision,
+	gemiddelde_bereikbaarheidsindex double precision,
+	maximale_bereikbaarheidsindex double precision,
+	CONSTRAINT ontwikkellocaties_pkey PRIMARY KEY (sid)
 );
 
--- DROP TABLE IF EXISTS datasysteem.development_plans_summary CASCADE;
-CREATE TABLE datasysteem.development_plans_summary(
+-- DROP TABLE IF EXISTS datasysteem.overzicht_ontwikkellocaties CASCADE;
+CREATE TABLE datasysteem.overzicht_ontwikkellocaties(
 	sid serial NOT NULL,
-	plan_name varchar,
-	new_dwellings integer,
-	in_development_potential integer,
-	outside_development_potential integer,
-	CONSTRAINT development_locations_summary_pkey PRIMARY KEY (sid)
+	plan_naam varchar,
+	geplande_woningen integer,
+	in_onderbenut_bereijkbaar integer,
+	buiten_onderbenut_bereijkbaar integer,
+	CONSTRAINT overzicht_ontwikkellocaties_pkey PRIMARY KEY (sid)
 );
 
--- DROP TABLE IF EXISTS datasysteem.station_influence_overlap CASCADE;
-CREATE TABLE datasysteem.station_influence_overlap(
-	sid serial NOT NULL,
-	geom geometry(MultiPolygon,28992),
-	residents integer,
-	intensity integer,
-	station_names varchar,
-	station_number smallint,
-	cycle_routes varchar,
-	ov_routes varchar,
-	CONSTRAINT station_influence_overlap_pkey PRIMARY KEY (sid)
-);
-
--- DROP TABLE IF EXISTS datasysteem.important_locations CASCADE;
-CREATE TABLE datasysteem.important_locations(
+-- DROP TABLE IF EXISTS datasysteem.invloedsgebied_overlap CASCADE;
+CREATE TABLE datasysteem.invloedsgebied_overlap(
 	sid serial NOT NULL,
 	geom geometry(MultiPolygon,28992),
-	location_type varchar,
-	location_id varchar,
-	location_name varchar,
-	cycle_routes varchar,
-	ov_routes varchar,
-	CONSTRAINT important_locations_pkey PRIMARY KEY (sid)
+	inwoners integer,
+	intensiteit integer,
+	station_namen varchar,
+	station_aantal smallint,
+	fietsroutes_ids varchar,
+	ov_routes_ids varchar,
+	CONSTRAINT invloedsgebied_overlap_pkey PRIMARY KEY (sid)
 );
 
--- DROP TABLE IF EXISTS datasysteem.cycle_routes CASCADE;
-CREATE TABLE datasysteem.cycle_routes(
+-- DROP TABLE IF EXISTS datasysteem.belangrijke_locaties CASCADE;
+CREATE TABLE datasysteem.belangrijke_locaties(
+	sid serial NOT NULL,
+	geom geometry(MultiPolygon,28992),
+	locatie_id varchar,
+	location_naam varchar,
+	fietsroutes_ids varchar,
+	ov_routes_ids varchar,
+	CONSTRAINT belangrijke_locaties_pkey PRIMARY KEY (sid)
+);
+
+-- DROP TABLE IF EXISTS datasysteem.regionale_voorzieningen CASCADE;
+CREATE TABLE datasysteem.regionale_voorzieningen(
+	sid serial NOT NULL,
+	geom geometry(MultiPoint,28992),
+	locatie_id varchar,
+	location_naam varchar,
+	fietsroutes_ids varchar,
+	ov_routes_ids varchar,
+	CONSTRAINT regionale_voorzieningen_pkey PRIMARY KEY (sid)
+);
+
+-- DROP TABLE IF EXISTS datasysteem.fietsroutes CASCADE;
+CREATE TABLE datasysteem.fietsroutes(
 	sid serial NOT NULL,
 	geom geometry(Linestring,28992),
 	route_id varchar,
-	route_name varchar,
-	route_intensity smallint,
-	CONSTRAINT cycle_routes_pkey PRIMARY KEY (sid)
+	route_naam varchar,
+	route_intensiteit smallint,
+	CONSTRAINT fietsroutes_pkey PRIMARY KEY (sid)
 );
 
 -- DROP TABLE IF EXISTS datasysteem.ov_routes CASCADE;
@@ -149,26 +160,25 @@ CREATE TABLE datasysteem.ov_routes(
 	sid serial NOT NULL,
 	geom geometry(Linestring,28992),
 	route_id varchar,
-	route_name varchar,
-	route_mode varchar,
-	trein_type varchar,
-	route_frequency double precision,
+	route_naam varchar,
+	route_modaliteit varchar,
+	route_frequentie double precision,
 	CONSTRAINT ov_routes_pkey PRIMARY KEY (sid)
 );
 
--- DROP TABLE IF EXISTS datasysteem.isochrones CASCADE;
-CREATE TABLE datasysteem.isochrones(
+-- DROP TABLE IF EXISTS datasysteem.isochronen CASCADE;
+CREATE TABLE datasysteem.isochronen(
 	sid serial NOT NULL,
 	geom geometry(MultiPolygon,28992),
-	stop_id varchar,
-	stop_name varchar,
-	travel_mode varchar,
-	distance smallint,
-	CONSTRAINT isochrones_pkey PRIMARY KEY (sid)
+	halte_id varchar,
+	halte_naam varchar,
+	modaliteit varchar,
+	isochroon_afstand smallint,
+	CONSTRAINT isochronen_pkey PRIMARY KEY (sid)
 );
 
--- DROP TABLE IF EXISTS datasysteem.ov_stops CASCADE;
-CREATE TABLE datasysteem.ov_stops(
+-- DROP TABLE IF EXISTS datasysteem.ov_haltes CASCADE;
+CREATE TABLE datasysteem.ov_haltes(
 	sid serial NOT NULL,
 	geom geometry(Point,28992),
 	stop_id varchar,
@@ -179,44 +189,30 @@ CREATE TABLE datasysteem.ov_stops(
 	metro boolean,
 	trein boolean,
 	bus boolean,
-	veerboot boolean,
+	ferry boolean,
 	bus_ochtendspits double precision,
-	bus_middagdal double precision,
+	bus_daluren double precision,
 	bus_avondspits double precision,
 	tram_ochtendspits double precision,
-	tram_middagdal double precision,
+	tram_daluren double precision,
 	tram_avondspits double precision,
 	metro_ochtendspits double precision,
-	metro_middagdal double precision,
+	metro_daluren double precision,
 	metro_avondspits double precision,
-	veerboot_ochtendspits double precision,
-	veerboot_middagdal double precision,
-	veerboot_avondspits double precision,
+	ferry_ochtendspits double precision,
+	ferry_daluren double precision,
+	ferry_avondspits double precision,
 	trein_ochtendspits double precision,
-	trein_middagdal double precision,
+	trein_daluren double precision,
 	trein_avondspits double precision,
 	hsl_ochtendspits double precision,
-	hsl_middagdal double precision,
+	hsl_daluren double precision,
 	hsl_avondspits double precision,
 	ic_ochtendspits double precision,
-	ic_middagdal double precision,
+	ic_daluren double precision,
 	ic_avondspits double precision,
 	spr_ochtendspits double precision,
-	spr_middagdal double precision,
+	spr_daluren double precision,
 	spr_avondspits double precision,
-	CONSTRAINT ov_stops_pkey PRIMARY KEY (sid)
-);
-
--- DROP TABLE IF EXISTS datasysteem.ov_links CASCADE;
-CREATE TABLE datasysteem.ov_links(
-	sid serial NOT NULL,
-	geom geometry(LineString,28992),
-	origin_stop_id varchar,
-	destination_stop_id varchar,
-	link_mode varchar,
-	trein_type varchar,
-	ochtendspits double precision,
-	middagdal double precision,
-	avondspits double precision,
-	CONSTRAINT ov_links_pkey PRIMARY KEY (sid)
+	CONSTRAINT ov_haltes_pkey PRIMARY KEY (sid)
 );
